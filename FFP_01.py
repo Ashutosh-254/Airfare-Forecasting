51c0039d1b5d86f68bc9edcfd8505932ddbba737
Ashutosh D <90883668+Ashutosh-254@users.noreply.github.com>
2023-12-15 01:42:38 +0530

    FFP_01.py

--- FFP_01.py
+++ FFP_01.py
@@ -3,24 +3,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
-
 #Loading the Dataset
 data = pd.read_csv('your_dataset.csv')
-
 #Column Names
 X = data.drop('Airfare_Price', axis=1)
 y = data['Airfare_by_Carrier']
 
-# Split the dataset into training and testing sets
+#Set Testing size=0.2
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Perform feature engineering (you can customize this based on your dataset)
-# For example, you can add new features, handle missing values, or scale numerical features
-
-# Create a Random Forest Regressor model
+#Creating a Random Forest Regressor model
 rf_model = RandomForestRegressor()
 
-# Define hyperparameters for tuning
+#Tuning Process
 param_grid = {
     'n_estimators': [50, 100, 200],
     'max_depth': [None, 10, 20],
@@ -28,20 +23,18 @@ param_grid = {
     'min_samples_leaf': [1, 2, 4]
 }
 
-# Use GridSearchCV for hyperparameter tuning
+#Implement GridSearchCV 
 grid_search = GridSearchCV(rf_model, param_grid, cv=5, scoring='neg_mean_absolute_error')
 grid_search.fit(X_train, y_train)
-
-# Get the best hyperparameters from the grid search
+#Obtain the best hyperparameters
 best_params = grid_search.best_params_
 
-# Train the model with the best hyperparameters
+#Train the model 
 best_rf_model = RandomForestRegressor(**best_params, random_state=42)
 best_rf_model.fit(X_train, y_train)
-
-# Make predictions on the test set
+#Making Predictions
 predictions = best_rf_model.predict(X_test)
 
-# Evaluate the model performance
+#Performance Evaluation
 mae = mean_absolute_error(y_test, predictions)
 print(f'Mean Absolute Error: {mae}')
